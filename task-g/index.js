7aa942338aef37b8f33967859554117f2ca37a27
chachouman <nassim.boudekhani@centria.fi>
2025-11-05 15:29:13 +0200

    task-h installed npm and tailwindcss

--- task-g/index.js
+++ task-g/index.js
@@ -53,76 +53,73 @@ document.addEventListener("DOMContentLoaded", () => {
 		// Phone number must be of length 8 < < 15
 		const phone = phoneInput.value.trim();
 		if (!phone) {
-            document.getElementById("telError").innerHTML = "Please enter your phone number";
-            console.error("ERROR: No email was given");
-            return;
-        }
-        if (phone.length < 8) {
-            document.getElementById("telError").innerHTML = "The phone number is too short";
-            console.error("ERROR: Invalid phone number: too short");
-            return;
-        }
-        if (phone.length > 16) {
-            document.getElementById("telError").innerHTML = "The phone number is too long";
-            console.error("ERROR: Invalid phone number: too long");
-            return;
-        }
-        if (phone.length == 16 && phone[0] != "+") {
-            document.getElementById("telError").innerHTML = "The phone number is too long";
-            console.error("ERROR: Invalid phone number: too long");
-            return;
-        }
-        console.info("INFO: Phone number was correctly given");
-        document.getElementById("telError").innerHTML = "";
+			document.getElementById("telError").innerHTML = "Please enter your phone number";
+			console.error("ERROR: No email was given");
+			return;
+		}
+		if (phone.length < 8) {
+			document.getElementById("telError").innerHTML = "The phone number is too short";
+			console.error("ERROR: Invalid phone number: too short");
+			return;
+		}
+		if (phone.length > 16) {
+			document.getElementById("telError").innerHTML = "The phone number is too long";
+			console.error("ERROR: Invalid phone number: too long");
+			return;
+		}
+		if (phone.length == 16 && phone[0] != "+") {
+			document.getElementById("telError").innerHTML = "The phone number is too long";
+			console.error("ERROR: Invalid phone number: too long");
+			return;
+		}
+		console.info("INFO: Phone number was correctly given");
+		document.getElementById("telError").innerHTML = "";
 
-        // Date of birth must be between 1900 (125 years old) and 13 years ago (French regulation)
-        const birth = dateInput.value;
-        if (!birth) {
-            document.getElementById("dateError").innerHTML = "Please enter a birth date";
-            console.error("ERROR: No birth date was given");
-            return;
-        }
-        const dateComponents = birth.split("-").map((x) => parseInt(x));
-        if (dateComponents[0] < 1900) {
-            document.getElementById("dateError").innerHTML = "Please enter a real birth date";
-            console.error("ERROR: Birth date is not real");
-            return;
-        }
-        if (dateComponents[0] > new Date().getFullYear() - 13) {
-            document.getElementById("dateError").innerHTML = "You are too young to use the website";
-            console.error("ERROR: User is too young");
-            return;
-        }
-        console.info("INFO: Birth date was correctly given");
-        document.getElementById("dateError").innerHTML = "";
+		// Date of birth must be between 1900 (125 years old) and 13 years ago (French regulation)
+		const birth = dateInput.value;
+		if (!birth) {
+			document.getElementById("dateError").innerHTML = "Please enter a birth date";
+			console.error("ERROR: No birth date was given");
+			return;
+		}
+		const dateComponents = birth.split("-").map((x) => parseInt(x));
+		if (dateComponents[0] < 1900) {
+			document.getElementById("dateError").innerHTML = "Please enter a real birth date";
+			console.error("ERROR: Birth date is not real");
+			return;
+		}
+		if (dateComponents[0] > new Date().getFullYear() - 13) {
+			document.getElementById("dateError").innerHTML = "You are too young to use the website";
+			console.error("ERROR: User is too young");
+			return;
+		}
+		console.info("INFO: Birth date was correctly given");
+		document.getElementById("dateError").innerHTML = "";
 
+		// Append the Data to new row
+		const row = document.createElement("tr");
 
+		const nameCell = document.createElement("td");
+		nameCell.textContent = fullName;
+		row.appendChild(nameCell);
 
-        // Append the Data to new row
-        const row = document.createElement("tr");
+		const emailCell = document.createElement("td");
+		emailCell.textContent = email;
+		row.appendChild(emailCell);
 
-        const nameCell = document.createElement("td");
-        nameCell.textContent = fullName;
-        row.appendChild(nameCell);
+		const telCell = document.createElement("td");
+		telCell.textContent = phone;
+		row.appendChild(telCell);
 
-        const emailCell = document.createElement("td");
-        emailCell.textContent = email;
-        row.appendChild(emailCell);
+		const birthdateCell = document.createElement("td");
+		birthdateCell.textContent = birth;
+		row.appendChild(birthdateCell);
 
-        const telCell = document.createElement("td");
-        telCell.textContent = phone;
-        row.appendChild(telCell);
+		const timeStampCell = document.createElement("td");
+		timeStampCell.textContent = timeStamp.toGMTString();
+		row.appendChild(timeStampCell);
 
-        const birthdateCell = document.createElement("td");
-        birthdateCell.textContent = birth;
-        row.appendChild(birthdateCell);
-
-        const timeStampCell = document.createElement("td");
-        timeStampCell.textContent = timeStamp.toGMTString();
-        row.appendChild(timeStampCell);
-
-
-        table.appendChild(row);
+		table.appendChild(row);
 
 		// Reset form and focus
 
