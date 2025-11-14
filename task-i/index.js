66bc76f53dde5d8f40e5823f4e7ecf0bae96ea85
JosephCouprie <joseph.couprie@centria.fi>
2025-11-14 17:57:18 +0100

    task-i tests push

--- task-i/index.js
+++ task-i/index.js
@@ -4,10 +4,13 @@
  */
 
 var hero_section;
+var hero_sub_section;
 document.addEventListener("DOMContentLoaded", () => {
 	hero_section = document.getElementById("hero-section");
+	hero_sub_section = document.getElementById("hero-sub-section");
 });
 document.addEventListener("scroll", () => {
 	//console.log(window.scrollY);
 	hero_section.style.backgroundPosition = 100-window.scrollY/5 + "% 50%";
+	hero_sub_section.style.backgroundPosition = 100-window.scrollY/12 + "% 50%";
 });
