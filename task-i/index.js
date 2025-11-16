31e4e6dad7229b254fd59041295133a4e443356a
chachouman <nassim.boudekhani@centria.fi>
2025-11-16 15:37:01 +0200

    task-i back to top button

--- task-i/index.js
+++ task-i/index.js
@@ -9,18 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
 	hero_section = document.getElementById("hero-section");
 	hero_sub_section = document.getElementById("hero-sub-section");
 
-	document.querySelectorAll(".card").forEach(card => {
+	document.querySelectorAll(".card").forEach((card) => {
 		card.addEventListener("click", () => {
-    		card.classList.toggle("flipped");
+			card.classList.toggle("flipped");
 		});
 	});
-
+	document.addEventListener("scroll", () => {
+		//console.log(window.scrollY);
+		hero_section.style.backgroundPosition = 100 - window.scrollY / 5 + "% 50%";
+		var divider = window.innerHeight > window.innerWidth ? 45 : 25;
+		hero_sub_section.style.backgroundPosition = 100 - (window.scrollY + 385) / divider + "% 50%";
+	});
 });
-document.addEventListener("scroll", () => {
-	//console.log(window.scrollY);
-	hero_section.style.backgroundPosition = 100-window.scrollY/5 + "% 50%";
-	var divider = (window.innerHeight > window.innerWidth)? 45 : 25;
-	hero_sub_section.style.backgroundPosition = 100-(window.scrollY+385)/divider + "% 50%";
-});
-
-
